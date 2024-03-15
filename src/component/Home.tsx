02a9872d47a6189e9e81f60a5924ae3b661a9ec2
Somayah-Absi <somayahahmedabsi@gmail.com>
2024-03-15 04:02:29 +0300

    updage in styling and image

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,16 +1,19 @@
 import React from "react";
+import companyPeopleImage from "../Image/company-people-working-and-transferring-money.png";
+
 
 export const Home = () => {
   return (
     <div className="home-container">
       <h1>
-        welcome to our budget app <br/>
-         place which help you to control your money!{" "}
+        welcome to our budget app <br />
+        place which helps you to control your money!{" "}
       </h1>
       <img
         className="home-image"
-        src="./src/Image/company-people-working-and-transferring-money.png"
-      ></img>
+        src={companyPeopleImage}
+        alt="People working and transferring money"
+      />
     </div>
   );
 };
